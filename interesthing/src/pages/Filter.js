8f9b35bfc3be010f328b8b6eebeb59c0b3188fec
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2023-06-16 09:20:29 +0800

    fix dbConnect with try except

--- interesthing/src/pages/Filter.js
+++ interesthing/src/pages/Filter.js
@@ -69,7 +69,7 @@ const footer = (
 
 const Filter = () => {
   const fetchGroups = async () =>
-    await axios.get(`http://127.0.0.1:5000/?hobby=${search}`);
+    await axios.get(`http://127.0.0.1:8080/?hobby=${search}`);
 
   const { data, refetch } = useQuery("groupsData", fetchGroups, {
     refetchOnWindowFocus: false,
